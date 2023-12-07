07a1bd4fa364af00c54b04e9c6f03d9b7c1150fb
Matija Bečirević <matija.becirevic@gmail.com>
2023-12-07 23:43:21 +0100

    Naive solve 05.12.2023.p2

---
+++ 2023/5/p2/solution.js
@@ -0,0 +1,72 @@
+import * as fs from 'fs/promises';
+
+const regex = /(\b\d+\b)/g;
+
+const parseMaps = (lines) => {
+    const mapLines = lines.slice(3).filter(line => line);
+    const maps = [];
+    let currentMap = [];
+
+    for (let i = 0; i < mapLines.length; i++) {
+        const line = mapLines[i]
+
+        if (line[line.length - 1] === ':') {
+            maps.push(currentMap);
+            currentMap = [];
+            continue;
+        }
+
+        const values = line.match(regex).map(Number);
+        currentMap.push({
+            d: values[0],
+            s: values[1],
+            r: values[2]
+        });
+    }
+
+    maps.push(currentMap);
+
+    return maps;
+}
+
+const solve = async (fileName) => {
+    const fileContent = await fs.readFile(fileName, { encoding: 'utf8' });
+    const lines = fileContent.split('\r\n');
+
+    const seedRanges = lines[0].match(regex).map(Number);
+    const seeds = [];
+
+    for (let i = 0; i <= seedRanges.length / 2; i++) {
+        const newSeeds = Array.from({ length: seedRanges[i * 2 + 1] }, (_, index) => seedRanges[i * 2] + index);
+        seeds.push(...newSeeds);
+    }
+
+    const maps = parseMaps(lines);
+    const locations = [];
+
+    for (let i = 0; i < seeds.length; i++) {
+        let value = seeds[i];
+
+        for (let j = 0; j < maps.length; j++) {
+            const map = maps[j];
+
+            for (let k = 0; k < map.length; k++) {
+                const { d, s, r } = map[k];
+
+                if (value >= s && value < s + r) {
+                    value = value + (d - s);
+                    break;
+                }
+            }
+        }
+
+        locations.push(value);
+    }
+
+    const closestLocation = locations.reduce((acc, curr) => acc > curr ? curr : acc, Infinity);
+
+    return closestLocation;
+}
+
+const solution = await solve('testCase');
+console.log(solution);
