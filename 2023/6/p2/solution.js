fb2b09330218bcea2daae0a80349159f16bd7fcc
Matija Bečirević <matija.becirevic@gmail.com>
2023-12-08 18:19:23 +0100

    Optimize solution #3 06.12.2023.p2

--- 2023/6/p2/solution.js
+++ 2023/6/p2/solution.js
@@ -8,16 +8,56 @@ const checkStrategyAgainstRecord = (totalTime, buttonReleaseTime, record) => {
     return distance > record;
 }
 
-const calculateWinOptions = (time, record) => {
+const binarySearch = (time, record) => {
+    // Assuming that it is always possible to break the record
+    // Strategy where the button is held for exactly half the time results in furthest distance traveled
+    // Thus, starting point is half the time
+    let lowerBoundary = Math.ceil(time * 0.5);
+    let nextValue = Math.ceil(lowerBoundary * 0.5);
+    let step = Math.ceil(nextValue * 0.5);
+
+    while (true) {
+        const isRecordBroken = checkStrategyAgainstRecord(time, nextValue, record);
+        if (isRecordBroken) {
+            lowerBoundary = nextValue;
+            nextValue = nextValue - step;
+            step = Math.floor((step + 1) * 0.5);
+        } else {
+            nextValue = nextValue + step;
+            step = Math.floor((step - 1) * 0.5);
+        }
+
+        if (step === 0)
+            break;
+    }
+
+    return lowerBoundary;
+}
+
+const linearSearch = (time, record) => {
     let lowerBoundary = -1;
 
-    for (let buttonReleaseTime = 1; buttonReleaseTime < time; buttonReleaseTime++) {
+    for (let buttonReleaseTime = 1; buttonReleaseTime < time / 2; buttonReleaseTime++) {
         const isWinningStrategy = checkStrategyAgainstRecord(time, buttonReleaseTime, record);
 
         if (isWinningStrategy) {
             lowerBoundary = buttonReleaseTime;
             break;
         }
+    }
+
+    return lowerBoundary;
+}
+
+const calculateWinOptions = (time, record) => {
+    const binarySearchTimeComplexity = Math.log2(time);
+    const linearSearchTimeComplexity = time / 2 - 1;
+    let lowerBoundary = null;
+
+    if (binarySearchTimeComplexity > linearSearchTimeComplexity) {
+        lowerBoundary = linearSearch(time, record)
+    } else {
+        lowerBoundary = binarySearch(time, record);
     }
 
     return time - (lowerBoundary * 2) + 1;
