43ed4452f07ef1eefdf5414aaee0584540f8fbc7
Matija Bečirević <matija.becirevic@gmail.com>
2023-12-10 20:47:50 +0100

    Solve 09.12.2023.p2

---
+++ 2023/9/p2/solution.js
@@ -0,0 +1,68 @@
+import * as fs from 'fs/promises';
+
+const calculateHistorySeqeuence = (history) => {
+    const result = {
+        sequence: [],
+        isLast: true
+    };
+
+    for (let i = 1; i < history.length; i++) {
+        const difference = history[i] - history[i - 1];
+        result.sequence.push(difference);
+
+        if (difference !== 0) {
+            result.isLast = false;
+        }
+    }
+
+    return result;
+};
+
+const calculateSequences = (history) => {
+    let sequences = [history];
+    let lastSequence = sequences[sequences.length - 1];
+    let isLast = false;
+
+    while (!isLast) {
+        const nextSequence = calculateHistorySeqeuence(lastSequence);
+        lastSequence = nextSequence.sequence;
+        isLast = nextSequence.isLast;
+        sequences = [lastSequence, ...sequences];
+    }
+
+    return sequences;
+};
+
+const fillNextSequenceValue = (previousSequence, sequence) => {
+    const lastValue = previousSequence[0];
+    const nextValue = sequence[0] - lastValue;
+    return sequence.unshift(nextValue);
+};
+
+const calculateNextHistoryValue = (history) => {
+    history[0].push(0);
+
+    for (let i = 1; i < history.length; i++) {
+        fillNextSequenceValue(history[i - 1], history[i]);
+    }
+
+    const lastSequence = history[history.length - 1];
+    const lastValue = lastSequence[0];
+
+    return lastValue;
+}
+
+const solve = async (fileName) => {
+    const fileContent = await fs.readFile(fileName, { encoding: 'utf8' });
+    const lines = fileContent.split('\r\n');
+
+    const histories = lines.map(line => line.split(' ').map(Number));
+    const historiesWithSequences = histories.map(history => calculateSequences(history));
+    const newValues = historiesWithSequences.map(x => calculateNextHistoryValue(x));
+    const result = newValues.reduce((acc, value) => acc + value, 0);
+
+    return result;
+}
+
+const solution = await solve('testCase');
+console.log(solution);
