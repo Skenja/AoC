8d0d3cb5ce41c492fceb7d98536f62f806f5855a
Matija Bečirević <matija.becirevic@gmail.com>
2023-12-27 21:02:54 +0100

    WIP: settling bricks 22.12.2023.p1

--- 2023/22/p1/solution.js
+++ 2023/22/p1/solution.js
@@ -2,8 +2,7 @@ import * as fs from 'fs/promises';
 
 const regex = /(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)/;
 
-const solve = async (fileName) => {
-    const fileContent = await fs.readFile(fileName, { encoding: 'utf8' });
+const parseBricks = (fileContent) => {
     const bricks = fileContent
         .split('\r\n')
         .map(line => line
@@ -16,17 +15,29 @@ const solve = async (fileName) => {
             x2, y2, z2: z2 - 1
         }));
 
+    return bricks;
+}
+
+const getSpaceSizeFromBricks = (bricks) => {
     const spaceSize = bricks.reduce((acc, brick) => ({
         x: Math.max(acc.x, brick.x2 + 1),
         y: Math.max(acc.y, brick.y2 + 1),
         z: Math.max(acc.z, brick.z2 + 1)
     }), { x: 0, y: 0, z: 0 });
 
+    return spaceSize
+}
+
+const generate3DSpace = (spaceSize) => {
     const space = Array.from({ length: spaceSize.z }, () =>
         Array.from({ length: spaceSize.y }, () =>
             Array(spaceSize.x).fill(null)
         ));
 
+    return space;
+}
+
+const fillSpaceWithBricks = (space, bricks) => {
     for (const brick of bricks) {
         for (let z = brick.z1; z <= brick.z2; z++) {
             for (let y = brick.y1; y <= brick.y2; y++) {
@@ -37,7 +48,29 @@ const solve = async (fileName) => {
         }
     }
 
-    return space
+    return space;
+}
+
+const settleBricks = (spaceSize, space, bricks) => {
+    const settledSpace = generate3DSpace(spaceSize);
+
+    // start at bottom layer
+    // for every empty space in the layer find the first brick directly above it
+    // check if the entire brick is visible from current layer
+    // if so, reposition it to start in the current layer
+
+    return settledSpace;
+}
+
+const solve = async (fileName) => {
+    const fileContent = await fs.readFile(fileName, { encoding: 'utf8' });
+    const bricks = parseBricks(fileContent);
+    const spaceSize = getSpaceSizeFromBricks(bricks);
+    const space = generate3DSpace(spaceSize);
+    const filledSpace = fillSpaceWithBricks(space, bricks);
+    const settledSpace = settleBricks(spaceSize, filledSpace, bricks);
+
+    return settledSpace
 }
 
 const solution = await solve('testCase');
