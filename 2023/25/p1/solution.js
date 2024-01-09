c99599d829e98d55161f95b9ba32e3f7a00cf405
Matija Bečirević <matija.becirevic@gmail.com>
2024-01-09 17:16:52 +0100

    Solve 25.12.2023.p1

--- 2023/25/p1/solution.js
+++ 2023/25/p1/solution.js
@@ -2,126 +2,124 @@ import * as fs from 'fs/promises';
 
 const regex = /\b([a-z]+?):?\b/g;
 
-const getVertices = (fileContent) => {
-    const input = fileContent
+const getGraph = (fileContent) => {
+    const vertices = fileContent
         .split('\r\n')
         .map(line => [...line.matchAll(regex)])
         .map(matches => matches.map(match => match[1]))
         .map(([vertex, ...connectedVertices]) => ([vertex, connectedVertices]));
 
-    const vertices = new Map();
+    const graph = [];
+    const verticesIndexSet = new Set();
 
-    for (const [vertex, connectedVertices] of input) {
-        if (!vertices.has(vertex)) {
-            vertices.set(vertex, []);
-        }
+    for (const [source, targets] of vertices) {
+        verticesIndexSet.add(source);
 
-        for (const connectedVertex of connectedVertices) {
-            if (!vertices.has(connectedVertex)) {
-                vertices.set(connectedVertex, []);
-            }
+        for (const target of targets) {
+            verticesIndexSet.add(target);
 
-            vertices.get(vertex).push(connectedVertex);
-            vertices.get(connectedVertex).push(vertex);
+            const edge = [source, target].sort();
+            graph.push(edge);
         }
     }
 
-    return vertices;
+    const verticesIndex = [...verticesIndexSet];
+
+    return [graph, verticesIndex];
 }
 
-const kargers = (vertices) => {
-    const graph = new Map(JSON.parse(JSON.stringify([...vertices])));
-    let verticesIndex = [...graph.keys()];
+const getRandomNodePair = (verticesIndex) => {
+    let nodes = [0, 0];
 
-    while (graph.size > 2) {
-        const vertex1 = verticesIndex[Math.floor(Math.random() * verticesIndex.length)];
-        const connectedVertices1 = graph.get(vertex1);
-        const vertex2 = connectedVertices1[Math.floor(Math.random() * connectedVertices1.length)];
-        const connectedVertices2 = graph.get(vertex2);
-
-        const filteredConnectedVertices2 = connectedVertices2.filter(x => x !== vertex1);
-
-        connectedVertices1.splice(connectedVertices1.indexOf(vertex2), 1, ...filteredConnectedVertices2);
-        graph.set(vertex1, [...new Set(connectedVertices1)])
-
-        for (const connectedVertex of filteredConnectedVertices2) {
-            const connectedVertices = graph.get(connectedVertex);
-            connectedVertices.splice(connectedVertices.indexOf(vertex2), 1, vertex1);
-            graph.set(connectedVertex, [...new Set(connectedVertices)])
-        }
-
-        graph.delete(vertex2);
-        verticesIndex = [...graph.keys()];
+    while (nodes[0] === nodes[1]) {
+        nodes = [
+            verticesIndex[Math.floor(verticesIndex.length * Math.random())],
+            verticesIndex[Math.floor(verticesIndex.length * Math.random())]
+        ];
     }
 
-    return [...graph].map(x => x[0]);
-}
+    return nodes;
+};
 
-const buildHistogram = (vertices) => {
-    const histogram = new Map();
-
-    const n = Math.ceil(vertices.size * 0.5 * Math.log(vertices.size));
-
-    for (let i = 0; i < n; i++) {
-        if (i % 100 === 0) console.log(i, '/', n)
-
-        const last2 = kargers(vertices);
-        const key = last2.sort().join('-');
-
-        if (!histogram.has(key)) {
-            histogram.set(key, 0);
-        }
-
-        histogram.set(key, histogram.get(key) + 1);
-    }
-
-    return [...histogram.entries()]
-        .sort((a, b) => b[1] - a[1])
-        .slice(0, 5)
-}
-
-const walkGraph = (vertices, start) => {
-    const visited = new Set();
+const getShortestPath = (graph, start, end) => {
+    const visited = new Map([
+        [start, [start]]
+    ]);
     const queue = [start];
 
     while (queue.length > 0) {
         const vertex = queue.shift();
+        const connectedVertices = graph
+            .filter(edge => edge.includes(vertex))
+            .map(edge => edge.filter(node => node !== vertex)[0]);
 
-        if (visited.has(vertex)) {
-            continue;
-        }
+        for (const connectedVertex of connectedVertices) {
+            if (visited.has(connectedVertex)) {
+                continue;
+            }
 
-        visited.add(vertex);
+            const path = [...visited.get(vertex), connectedVertex];
 
-        for (const connectedVertex of vertices.get(vertex)) {
+            if (connectedVertex === end) {
+                return [path, null];
+            }
+
             queue.push(connectedVertex);
+            visited.set(connectedVertex, path);
         }
     }
 
-    return visited.size;
+    // If no path found, return cut size
+    return [null, visited.size];
+}
+
+const buildHistogram = (graph, verticesIndex) => {
+    const histogram = new Map();
+
+    for (let run = 0; run < 3; run++) {
+        const randomNodePair = getRandomNodePair(verticesIndex);
+        const [path, _] = getShortestPath(graph, ...randomNodePair)
+
+        while (path.length > 1) {
+            const edgeKey = path
+                .splice(0, 2, path[1])
+                .sort()
+                .toString();
+
+            if (!histogram.has(edgeKey)) {
+                histogram.set(edgeKey, 0);
+            }
+
+            histogram.set(edgeKey, histogram.get(edgeKey) + 1);
+        }
+    }
+
+    return [...histogram.entries()]
+        .sort((a, b) => b[1] - a[1])
+    [0][0];
+}
+
+const calculateCutSize = (graph, verticesIndex) => {
+    while (true) {
+        const maxEdge = buildHistogram(graph, verticesIndex);
+
+        const edgeIndex = graph.findIndex(edge => edge.toString() === maxEdge);
+        graph.splice(edgeIndex, 1);
+
+        const [_, cutSize] = getShortestPath(graph, ...maxEdge.split(','));
+
+        if (cutSize) {
+            return cutSize * (verticesIndex.length - cutSize);
+        }
+    }
 }
 
 const solve = async (fileName) => {
     const fileContent = await fs.readFile(fileName, { encoding: 'utf8' });
-    const vertices = getVertices(fileContent);
-    const top3 = buildHistogram(vertices);
+    const [graph, verticesIndex] = getGraph(fileContent);
+    const cutSize = calculateCutSize(graph, verticesIndex);
 
-    console.log(top3)
-
-    const top3Edges = top3.map(x => x[0].split('-'));
-
-    for (const [vertex1, vertex2] of top3Edges) {
-        const connectedVertices1 = vertices.get(vertex1);
-        connectedVertices1.splice(connectedVertices1.indexOf(vertex2), 1);
-
-        const connectedVertices2 = vertices.get(vertex2);
-        connectedVertices2.splice(connectedVertices2.indexOf(vertex1), 1);
-    }
-
-    const size1 = walkGraph(vertices, top3Edges[0][0]);
-    const size2 = walkGraph(vertices, top3Edges[0][1]);
-
-    return [size1, size2, size1 * size2];
+    return cutSize;
 }
 
 const solution = await solve('testCase');
